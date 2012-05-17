a671c739e0d1da8d721cfcd6bafefb9e2a378970
suyashrails <suyash.ps@gmail.com>
2012-05-18 01:48:38 +0530

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuneProfiler::Application.config.secret_token = 'fb5eea6bb87ae4aea3d9be4663ef9f7ede5e677990466467a3f28adb58accaef2be9e59c5fb7f0523084079179f79432dfa8392291653e81a6218d2ffc424541'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TuneProfiler::Application.config.session_store :cookie_store, key: '_tune_profiler_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TuneProfiler::Application.config.session_store :active_record_store
