a021d133e17d0c58debf3d00dab9b483ad39f4de
suyashrails <suyash.ps@gmail.com>
2012-06-20 00:50:28 +0530

    heroku push fix 8

--- config/application.rb
+++ config/application.rb
@@ -65,5 +65,8 @@ module TuneProfiler
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Attempt to prevent heroku deploy error
+    config.assets.initialize_on_precompile = false
   end
 end
